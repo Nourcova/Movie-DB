b2be8652acaf817f97fb96d4520f201f8be82118
Nourcova <ibrahamnour22@gmail.com>
2021-12-31 20:48:27 +0200

    step 4 -Let's complicate the API

--- index.js
+++ index.js
@@ -6,13 +6,25 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/test',(req,res)=>{
-    res.json({"status":200,"message":"Ok"})
+    res.json({status:200,message:"Ok"})
 })
 
 app.get('/time',(req,res)=>{
     let hours=new Date().getHours();
     let minutes = new Date().getMinutes();
-    res.json({"status":200,"message":`${hours} : ${minutes}`})
+    res.json({status:200,message:`${hours} : ${minutes}`})
+})
+
+app.get('/search',(req,res)=>{
+    console.log(req.query.s);
+    req.query.s ? res.json({status:200,message:"Ok",data:req.query.s}) : res.status(500).json({status:500,error:true, message:"you have to provide a search"})
+})
+
+app.get('/hello/:id',(req,res)=>{
+    res.json({status:200,message:req.params.id}) 
+})
+app.get('/hello',(req,res)=>{
+    res.json({status:200,message:"hello"})
 })
 
 app.listen(3000,() => {

aeda043b9c52684f00635537032e1d8e212ea020
Nourcova <ibrahamnour22@gmail.com>
2021-12-31 21:04:49 +0200

    Step 5 - Set up the basis of CRUD

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require ('express');
 const app = express();
 
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+]
+
 app.get('/',(req,res)=>{
     res.sendStatus(200);
+})
+
+app.get('/hello/:id',(req,res)=>{
+    res.json({status:200,message:req.params.id}) 
+})
+app.get('/hello',(req,res)=>{
+    res.json({status:200,message:"hello"})
 })
 
 app.get('/test',(req,res)=>{
@@ -20,11 +34,17 @@ app.get('/search',(req,res)=>{
     req.query.s ? res.json({status:200,message:"Ok",data:req.query.s}) : res.status(500).json({status:500,error:true, message:"you have to provide a search"})
 })
 
-app.get('/hello/:id',(req,res)=>{
-    res.json({status:200,message:req.params.id}) 
+app.get('/movies/create',(req,res) => {
+
 })
-app.get('/hello',(req,res)=>{
-    res.json({status:200,message:"hello"})
+app.get('/movies/read',(req,res) => {
+    res.json({status:"200",message:movies})
+})
+app.get('/movies/update',(req,res) => {
+    
+})
+app.get('/movies/delete',(req,res) => {
+    
 })
 
 app.listen(3000,() => {

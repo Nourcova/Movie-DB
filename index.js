6110488329a480ff746b92b5f011a0a38bb91b51
Nourcova <ibrahamnour22@gmail.com>
2022-01-09 15:26:35 +0200

    fixig the schema

--- index.js
+++ index.js
@@ -14,23 +14,17 @@ mongoose
     .catch(err => console.log(err));
 
 const movieSchema = mongoose.Schema({
-    movieItems: [{
-        title: String,
-        year: Number,
-        rating: Number
-    }]
+    title: String,
+    year: Number,
+    rating: Number
 })
 
 const moviesDB = mongoose.model('moviesDB', movieSchema)
 
-const Movies = new moviesDB({
-    movieItems: [
-        { title: "Jaws", year: 1975, rating: 8 },
-        { title: "Avatar", year: 2009, rating: 7.8 },
-        { title: "Brazil", year: 1985, rating: 8 },
-        { title: "الإرهاب والكباب", year: 1992, rating: 6.2 },
-    ],
-})
+// moviesDB.create({  title: "Jaws", year: 1975, rating: 8 },
+//     { title: "Avatar", year: 2009, rating: 7.8 },
+//     { title: "Brazil", year: 1985, rating: 8 },
+//     { title: "الإرهاب والكباب", year: 1992, rating: 6.2 })
 
 
 
@@ -82,7 +76,6 @@ const movies =
     ]
 
 
-
 app.get('/movies/read', (req, res) => {
     console.log(req.params)
     moviesDB.find().then(data => {
@@ -95,76 +88,74 @@ app.get('/movies/read', (req, res) => {
 
 //step 6
 app.get('/movies/read/by-date', (req, res) => {
-    res.json({ status: 200, message: Movies.movieItems.sort((a, b) => b.year - a.year) })
+    moviesDB.find().then(data => {
+        res.status(200).json({ satus: 200, message: data.sort((a, b) => b.year - a.year) })
+    })
 })
 
 app.get('/movies/read/by-rating', (req, res) => {
-    res.json({ status: 200, message: Movies.movieItems.sort((a, b) => b.rating - a.rating) })
+    moviesDB.find().then(data => {
+        res.status(200).json({ satus: 200, message: data.sort((a, b) => b.rating - a.rating) })
+    })
 })
 
 app.get('/movies/read/by-title', (req, res) => {
-    res.json({
-        status: 200, message: Movies.movieItems.sort((a, b) => {
-            if (a.title.toLowerCase() < b.title.toLowerCase())
-                return -1
-            if ((a.title.toLowerCase() > b.title.toLowerCase()))
-                return 1
-            return 0
-            // a.title.toLowerCase() < b.title.toLowerCase() ? -1 : a.title.toLowerCase() > b.title.toLowerCase() ? 1 : 0
+    moviesDB.find().then(data => {
+        res.status(200).json({
+            status: 200, message: data.sort((a, b) => {
+                if (a.title.toLowerCase() < b.title.toLowerCase())
+                    return -1
+                if ((a.title.toLowerCase() > b.title.toLowerCase()))
+                    return 1
+                return 0
+            })
         })
     })
 })
 
 //step 7
 app.get('/movies/read/id/:id', (req, res) => {
-    let id = req.params.id
+    let id = req.params.id  
 
     moviesDB.findById(id).then(data => {
         res.status(200).json(data)
     }).catch(err => {
         res.status(404).json({ message: err })
     });
-    // if (id < movies.length) {
-    //     res.json({ status: 200, data: movies[id] })
-    // }
-    // else {
-    //     res.status(404).json({ status: 404, error: true, message: `the movie ${id} does not exist` })
-    // }
-
 })
 
 //step 8
-app.post('/movies/create', (req, res) => {
-    if (!req.query.rating) {
-        req.query.rating = 4;
-    }
-    let title = req.query.title;
-    let year = req.query.year;
-    let rating = req.query.rating;
+// app.post('/movies/create', (req, res) => {
+//     if (!req.query.rating) {
+//         req.query.rating = 4;
+//     }
+//     let title = req.query.title;
+//     let year = req.query.year;
+//     let rating = req.query.rating;
 
-    if (!title || !year || year.length != 4 || isNaN(year)) {
-        res.json({ status: 403, error: true, message: 'you cannot create a movie without providing a title and a year' })
-    }
+//     if (!title || !year || year.length != 4 || isNaN(year)) {
+//         res.json({ status: 403, error: true, message: 'you cannot create a movie without providing a title and a year' })
+//     }
 
-    else {
-        year = parseInt(year);
-        rating = parseInt(rating);
-        movies.push({ title, year, rating });
-        // res.json({ status: 200, data: movies });
-        console.log(req.body);
-        // const Movies = new moviesDB({
-        //     "title": req.body.title,
-        //     "year": req.body.year,
-        //     "rating": req.body.rating
-        // });
-        Movies.movieItems.push(req.body)
-        Movies.save().then(data => {
-            res.json(data)
-        }).catch(err => {
-            res.json({ message: err })
-        });
-    }
-})
+//     else {
+//         year = parseInt(year);
+//         rating = parseInt(rating);
+//         movies.push({ title, year, rating });
+//         // res.json({ status: 200, data: movies });
+//         console.log(req.body);
+//         // const Movies = new moviesDB({
+//         //     "title": req.body.title,
+//         //     "year": req.body.year,
+//         //     "rating": req.body.rating
+//         // });
+//         Movies.movieItems.push(req.body)
+//         Movies.save().then(data => {
+//             res.json(data)
+//         }).catch(err => {
+//             res.json({ message: err })
+//         });
+//     }
+// })
 
 //step 9
 app.delete('/movies/delete/:id', (req, res) => {
@@ -185,16 +176,7 @@ app.delete('/movies/delete/:id', (req, res) => {
 })
 
 //step 10
-app.patch('/movies/update/:id', (req, res) => {
-    // if (req.params.id > 0 && req.params.id < movies.length) {
-    //     if (req.query.title) movies[req.params.id].title = req.query.title;
-    //     if (req.query.year) movies[req.params.id].year = req.query.year;
-    //     if (req.query.rating) movies[req.params.id].rating = req.query.rating;
-    //     res.status(200).json({ status: 200, data: movies });
-    // }
-    // else {
-    //     res.status(404).json({ status: 404, error: true, message: 'the movie <ID> does not exist' })
-    // }
+app.put('/movies/update/:id', (req, res) => {
     let id = req.params.id;
 
     if (req.query.title) {
